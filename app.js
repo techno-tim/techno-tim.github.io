81df82ebe86b8930a9a090b68b0edb4162a2fdd7
Timothy Stewart <timothystewart6@gmail.com>
2022-02-05 22:26:41 -0600

    fix(pwa): update scripts

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 ---
-  layout: compress
+layout: compress
+permalink: '/app.js'
 ---
 
 /* Registering Service Worker */
-if ('serviceWorker' in navigator) {
+if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}');
 };
